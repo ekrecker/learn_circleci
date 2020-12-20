b1f072b8c7d0ea87dde8dcb0ac5b1a02a0579db0
kwgch <eiichi.kendoruki@gmail.com>
2020-12-20 21:43:37 +0900

    first commit

---
+++ test_calc.py
@@ -0,0 +1,10 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+    def test_plus_miss(self):
+        self.assertEqual(calc.plus(1,1), 3)
+    def test_plus(self):
+        self.assertEqual(calc.plus(3,5), 8)
+    def test_minus(self):
+        self.assertEqual(calc.minus(10,3), 7)
